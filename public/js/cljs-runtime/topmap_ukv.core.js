3f936de5de7f134f9148c0219f7182f742c91f89
ShevaGed <shevaged1990@gmail.com>
2026-03-25 17:43:30 +0200

    Add button count training 2

--- public/js/cljs-runtime/topmap_ukv.core.js
+++ public/js/cljs-runtime/topmap_ukv.core.js
@@ -1,10 +1,28 @@
 goog.provide('topmap_ukv.core');
-topmap_ukv.core.init = (function topmap_ukv$core$init(){
-console.log("ClojureScript add!");
+if((typeof topmap_ukv !== 'undefined') && (typeof topmap_ukv.core !== 'undefined') && (typeof topmap_ukv.core.counter !== 'undefined')){
+} else {
+topmap_ukv.core.counter = cljs.core.atom.cljs$core$IFn$_invoke$arity$1((0));
+}
+topmap_ukv.core.render_BANG_ = (function topmap_ukv$core$render_BANG_(){
+var display = document.getElementById("counter-display");
+return (display.innerText = cljs.core.deref(topmap_ukv.core.counter));
+});
+topmap_ukv.core.handle_click = (function topmap_ukv$core$handle_click(){
+cljs.core.swap_BANG_.cljs$core$IFn$_invoke$arity$2(topmap_ukv.core.counter,cljs.core.inc);
 
-return document.getElementById("app").insertAdjacentHTML("beforeend","<h1>Hello! ClojureScript are working.</h1>");
+return topmap_ukv.core.render_BANG_();
+});
+topmap_ukv.core.init = (function topmap_ukv$core$init(){
+var btn_11745 = document.getElementById("counter-btn");
+btn_11745.addEventListener("click",topmap_ukv.core.handle_click);
+
+topmap_ukv.core.render_BANG_();
+
+return console.log("Project Started!");
 });
 topmap_ukv.core.reload_BANG_ = (function topmap_ukv$core$reload_BANG_(){
+topmap_ukv.core.render_BANG_();
+
 return console.log("Code is apdate auto!");
 });
 
--- src/main/topmap_ukv/core.cljs
+++ src/main/topmap_ukv/core.cljs
@@ -1,10 +1,21 @@
 (ns topmap-ukv.core)
 
+(defonce counter (atom 0))
+
+(defn render! []
+  (let [display (js/document.getElementById "counter-display")]
+    (set! (.-innerText display) @counter)))
+
+(defn handle-click []
+  (swap! counter inc)
+  (render!))
+
 (defn init []
-  (js/console.log "ClojureScript add!")
-  (.insertAdjacentHTML (js/document.getElementById "app")
-                       "beforeend"
-                       "<h1>Hello! ClojureScript are working.</h1>"))
+  (let [btn (js/document.getElementById "counter-btn")]
+    (.addEventListener btn "click" handle-click))
+  (render!)
+  (js/console.log "Project Started!"))
 
 (defn ^:dev/after-load reload! []
+  (render!)
   (js/console.log "Code is apdate auto!"))
